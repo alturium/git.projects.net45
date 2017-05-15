b5bb5091181defe8653e342edd1cb4766693d53a
Michael Searson <michael.searson@navy.mil>
2017-05-15 09:11:55 -0700

    added merge script cmd

---
+++ merge_library.net.cmd
@@ -0,0 +1,8 @@
+
+#merges git.library.net (master) directly here
+# 'library' is temp name
+
+git remote add library https://github.com/alturium/git.library.net.git
+git fetch library
+git merge --allow-unrelated-histories library/master
+git remote remove library
